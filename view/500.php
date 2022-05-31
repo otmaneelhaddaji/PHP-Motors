ad1e0e630e864c29ef2f35f5340b1e1dd7d938b9
elhaddaji_Otmane <76048466+otmaneelhaddaji@users.noreply.github.com>
2022-05-31 07:47:52 +0100

    Update 500.php

--- view/500.php
+++ view/500.php
@@ -11,14 +11,14 @@
 
 <body>
     <div id="content">
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+
 
         <main>
             <h1>Server Error</h1>
             <p>Sorry, our server seems to be experiencing some technical difficulties.</p>
         </main>
 
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+
     </div>
 
 </body>
